9d1e2a99cc3bd2e410f2fed76f2741c373434fb6
Benjamin <benjaminjcharlton@gmail.com>
2021-03-28 16:52:08 +0400

    Bumped version

--- Globalization/GlobalizationApplicationBuilderExtensions.cs
+++ Globalization/GlobalizationApplicationBuilderExtensions.cs
@@ -1,23 +1,30 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Localization;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class GlobalizationApplicationBuilderExtensions
     {
-        private static RequestCulture BritishEnglish = new RequestCulture("en-GB");
-
         public static IApplicationBuilder UseRequestLocalization(
             this IApplicationBuilder app,
-            RequestCulture? defaultCulture = null,
+            RequestCulture defaultCulture,
             params RequestCulture[] supportedCultures)
         {
+            if (app is null)
+                throw new ArgumentNullException(nameof(app));
+
             return app.UseRequestLocalization(options =>
             {
-                options.DefaultRequestCulture = defaultCulture ?? BritishEnglish;
-                options.SupportedCultures = supportedCultures.Select(rc => rc.Culture).ToList();
-                options.SupportedUICultures = supportedCultures.Select(rc => rc.UICulture).ToList();
+                options.DefaultRequestCulture = defaultCulture ?? throw new ArgumentNullException(nameof(defaultCulture));
+
+                if(supportedCultures.IsNotNullOrEmpty())
+                {
+                    options.SupportedCultures = supportedCultures.Select(rc => rc.Culture).ToList();
+                    options.SupportedUICultures = supportedCultures.Select(rc => rc.UICulture).ToList();
+                }
             });
         }
     }
