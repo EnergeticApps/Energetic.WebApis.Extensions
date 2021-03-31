fa1e9c9cd198b57fd35202114657cd77c94a1bf2
Benjamin <benjaminjcharlton@gmail.com>
2021-03-31 18:38:41 +0400

    Stuff to make Swagger work with API versioning

--- Swagger/SwaggerServiceCollectionExtensions.cs
+++ Swagger/SwaggerServiceCollectionExtensions.cs
@@ -1,32 +1,60 @@
-﻿using Energetic.Reflection;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
-using Energetic.ValueObjects;
+using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
-using System.Linq;
-using Swashbuckle.AspNetCore.SwaggerGen;
-using Energetic.WebApis;
-using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
     public static class SwaggerServiceCollectionExtensions
-    {
+    {      
         public static IServiceCollection AddSwaggerGenForEveryApiVersion(this IServiceCollection services, IConfiguration configuration, params Type[] valueObjectMarkerTypes)
         {
+            // TODO: This method is so ugly. Refactor.
+
             services.MapValueObjectsToPrimitiveSchemaTypes(valueObjectMarkerTypes);
 
-            foreach (OpenApiInfo api in SwaggerHelper.GetApiVersions(configuration))
+            var apis = SwaggerHelper.GetApiVersions(configuration);
+
+            services.AddSwaggerGen(c =>
             {
-                services.AddSwaggerGen(c =>
+                c.IncludeXmlComments();
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. <br />
+                          Enter 'Bearer' [space] and then your token in the text input below.
+                          <br />Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement()
+                    {
+                            {
+                                new OpenApiSecurityScheme
+                                {
+                                    Reference = new OpenApiReference
+                                    {
+                                        Type = ReferenceType.SecurityScheme,
+                                        Id = "Bearer"
+                                    },
+                                    Scheme = "oauth2",
+                                    Name = "Bearer",
+                                    In = ParameterLocation.Header,
+                                },
+                                new List<string>()
+                            }
+                    });
+
+                foreach (OpenApiInfo api in SwaggerHelper.GetApiVersions(configuration))
                 {
                     c.SwaggerDoc(api.Version.ToLowerInvariant(), api);
-                    c.IncludeXmlComments();
-                });
-            }
+                }
+            });
 
             return services;
         }
@@ -38,7 +66,7 @@ namespace Microsoft.Extensions.DependencyInjection
 
             services.AddSwaggerGen(c =>
             {
-                foreach(var valueObjectType in valueObjectTypes)
+                foreach (var valueObjectType in valueObjectTypes)
                 {
                     Type valueObjectBaseType = valueObjectType.GetValueObjectBaseType();
                     Type primitiveType = valueObjectBaseType.GenericTypeArguments[1];
@@ -74,7 +102,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             var (schema, format) = GetOpenApiSchemaTypeClosestTo(dotNetType);
 
-            if(format is null)
+            if (format is null)
             {
                 return new OpenApiSchema { Type = schema };
             }
@@ -88,22 +116,31 @@ namespace Microsoft.Extensions.DependencyInjection
             {
                 case Type type when type == typeof(int):
                     return ("integer", "int32");
+
                 case Type type when type == typeof(long):
                     return ("integer", "int64");
+
                 case Type type when type == typeof(decimal):
                     return ("number", "float");
+
                 case Type type when type == typeof(double):
                     return ("number", "double");
+
                 case Type type when type == typeof(string):
                     return ("string", null);
+
                 case Type type when type == typeof(Guid):
                     return ("string", null);
+
                 case Type type when type == typeof(bool):
                     return ("boolean", null);
+
                 case Type type when type == typeof(DateTime):
                     return ("string", "date-time");
+
                 case Type type when type == typeof(DateTimeOffset):
                     return ("string", "date-time");
+
                 default:
                     throw new NotSupportedException($"Don't know which OpenApi primitive type schema is closest to {primitiveType.Name}.");
             }
